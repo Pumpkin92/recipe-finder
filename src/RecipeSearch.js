f108c2e9d0eb6bea7e3cb972b735ef7c0562c3dc
Pumpkin92 <77683675+Pumpkin92@users.noreply.github.com>
2022-04-03 18:12:25 +0100

    created initial search component and integrated API

---
+++ src/RecipeSearch.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Recipes from "./Recipes";
+
+export default function RecipeSearch() {
+  let [searchResult, setSearchResult] = useState("Pasta");
+  let [loaded, setLoaded] = useState(false);
+  let [recipes, setRecipes] = useState(null);
+
+  function handleUpdate(response) {
+    setRecipes(response.data.results);
+  }
+
+  function search() {
+    let apiKey = "16fc38902d074c0a8b4b6cde2b677493";
+    let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${searchResult}&apiKey=${apiKey}`;
+    axios.get(apiUrl).then(handleUpdate);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleSearchResult(event) {
+    setSearchResult(event.target.value);
+  }
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="container">
+        <section>
+          <h1>Recipe Finder</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleSearchResult}
+              placeholder="Search for an ingredient or recipe"
+            ></input>
+          </form>
+        </section>
+        <Recipes recipes={recipes} />
+      </div>
+    );
+  } else {
+    load();
+    return "loading";
+  }
+}
---
+++ src/Recipes.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function Recipes(props) {
+  console.log(props.recipes);
+  return (
+    <div>
+      <section></section>
+    </div>
+  );
+}
